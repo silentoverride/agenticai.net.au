29664940b36d8f5b838823bbf7250931255a4c46
silentoverride <170799280+silentoverride@users.noreply.github.com>
2026-05-07 16:00:14 +1000

    Set build.target to esnext to reduce esbuild transpilation
    Result: {"status":"keep","build_time":7.8}

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  build: {
+    target: 'esnext'
+  },
   ssr: {
     noExternal: [/^@clerk\//, 'cookie', 'devalue', 'set-cookie-parser', 'clsx', '@sveltejs/kit', '@sveltejs/kit/internal', '@sveltejs/kit/internal/server']
   }
